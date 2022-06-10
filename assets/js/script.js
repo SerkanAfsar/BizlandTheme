d3978331806d107ab14bbbf7791c4a4b2b91c7e2
CrimsonSky <serkan-afsar@hotmail.com>
2022-06-10 12:08:49 +0300

    Preloader Visibility And Opacity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 import AOS from 'aos';
 const loader = document.getElementById("preloader");
+const headerTop = document.getElementById("headerTop");
+const header = document.querySelector("header");
 window.onload = () => {
     setTimeout(() => {
-        loader.style.display = "none"
-    }, 200);
+        loader.classList.add("disabled");
+    }, 1500);
+}
+window.onscroll = (e) => {
+    if (window.scrollY >= headerTop.clientHeight) {
+        header.classList.add("sticky");
+        header.classList.add("shadow");
+    }
+    else {
+        header.classList.remove("sticky");
+    }
 }
 
 AOS.init({
